763ca84f3f81c6e01ad779eafb3ea10515f274e0
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2025-06-27 16:54:43 +0100

    make cache generic over k, v

--- server/src/cache.rs
+++ server/src/cache.rs
@@ -1,16 +1,20 @@
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::fmt::Display;
+use std::hash::Hash;
 use std::rc::Rc;
 
-struct Node {
-    v: u32,
-    next: Option<Rc<RefCell<Node>>>,
-    prev: Option<Rc<RefCell<Node>>>,
+struct Node<K, V> {
+    k: K,
+    v: V,
+    next: Option<Rc<RefCell<Node<K, V>>>>,
+    prev: Option<Rc<RefCell<Node<K, V>>>>,
 }
 
-impl Node {
-    fn new(v: u32) -> Self {
+impl<K, V> Node<K, V> {
+    fn new(k: K, v: V) -> Self {
         Self {
+            k,
             v,
             next: None,
             prev: None,
@@ -18,14 +22,14 @@ impl Node {
     }
 }
 
-struct LruCache {
-    m: HashMap<u32, Option<Rc<RefCell<Node>>>>,
-    head: Option<Rc<RefCell<Node>>>,
-    tail: Option<Rc<RefCell<Node>>>,
+struct LruCache<K, V> {
+    m: HashMap<K, Option<Rc<RefCell<Node<K, V>>>>>,
+    head: Option<Rc<RefCell<Node<K, V>>>>,
+    tail: Option<Rc<RefCell<Node<K, V>>>>,
     th: usize,
 }
 
-impl LruCache {
+impl<K: Eq + Hash + Clone, V: Display + Clone> LruCache<K, V> {
     /// creates a new `LruCache` with the given threshold `th`
     fn new(th: usize) -> Self {
         LruCache {
@@ -37,18 +41,21 @@ impl LruCache {
     }
 
     /// inserts value in `LruCache`, evicting lru entry if necessary
-    fn insert(&mut self, k: u32, v: u32) {
-        let new_node = Some(Rc::new(RefCell::new(Node::new(v))));
+    fn insert(&mut self, k: K, v: V) {
+        let new_node = Some(Rc::new(RefCell::new(Node::new(k.clone(), v))));
 
         // list empty
         if self.head.is_none() {
             self.head = new_node.clone();
             self.tail = self.head.clone();
         } else {
-            // check if threshold reached; removed lru (head)
+            // check if threshold reached; evict head (lru)
             if self.len() == self.th {
+                let head_k = self.head.clone().unwrap().borrow().k.clone();
+                // set head to next node
                 self.head = self.head.clone().unwrap().borrow().next.clone();
-                self.m.remove(&self.head.clone().unwrap().borrow().v);
+                // remove from map
+                self.m.remove(&head_k);
             }
             self.tail.clone().unwrap().borrow_mut().next = new_node.clone();
             new_node.clone().unwrap().borrow_mut().prev = self.tail.clone();
@@ -58,7 +65,8 @@ impl LruCache {
         self.m.insert(k, new_node.clone());
     }
 
-    fn remove(&mut self, k: u32) -> Option<u32> {
+    fn remove(&mut self, k: K) -> Option<V> where
+    K: Eq + Hash {
         if let Some(n) = self.remove_internal(k) {
             match Rc::try_unwrap(n) {
                 Ok(n) => {
@@ -75,7 +83,8 @@ impl LruCache {
     }
 
     /// removes an entry from the `LruCache`
-    fn remove_internal(&mut self, k: u32) -> Option<Rc<RefCell<Node>>> {
+    fn remove_internal(&mut self, k: K) -> Option<Rc<RefCell<Node<K, V>>>> where
+    K: Eq + Hash {
         if self.m.contains_key(&k) {
             let node = self.m.get(&k).unwrap().clone();
             self.m.remove(&k);
@@ -114,23 +123,23 @@ impl LruCache {
     }
 
     /// gets the value for the key `k` if present
-    fn get(&mut self, k: u32) -> Option<u32> {
+    fn get(&mut self, k: K) -> Option<V> {
         if self.m.contains_key(&k) {
             let node = self.remove_internal(k);
             self.tail.clone().unwrap().borrow_mut().next = node.clone();
             self.tail = node.clone();
-            Some(node.clone().unwrap().borrow().v)
+            Some(node.clone().unwrap().borrow().v.clone())
         } else {
             None
         }
     }
 
-    fn head(&self) -> u32 {
-        self.head.clone().unwrap().borrow().v
+    fn head(&self) -> V {
+        self.head.clone().unwrap().borrow().v.clone()
     }
 
-    fn tail(&self) -> u32 {
-        self.tail.clone().unwrap().borrow().v
+    fn tail(&self) -> V {
+        self.tail.clone().unwrap().borrow().v.clone()
     }
 
     fn len(&self) -> usize {
